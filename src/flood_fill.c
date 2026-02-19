475b1c7caf836b89258fe5a7a9c4a8f2087a7be0
Dancode52 <daniellanehart@gmail.com>
2026-02-19 16:12:42 +0100

    added test for funcs

--- src/flood_fill.c
+++ src/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:45:00 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/02/19 15:00:01 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/02/19 16:12:00 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,20 +63,23 @@ void	flood_fill(char **map, t_map_count *map_info, t_map_errors *error)
 {
 	size_t	row;
 	size_t	column;
+	char	**tmp;
 
-
+	tmp = map;
 	(void)error;
 	//set the point from which we will start the fill
 	set_start_point(&row, &column, map_info);
 	//have a function that sets the use looking for adjacent squares
 		//continue in a direcion until a wall is hit
 		//first test is with going right
-	go_right_till_wall(map, row, column);
-	printmap(map);
-	go_left_till_wall(map, row, column);
-	printmap(map);
-	go_up_till_wall(map, row, column);
-	printmap(map);
-	go_down_till_wall(map, row, column);
-	printmap(map);
+	go_right_till_wall(tmp, row, column);
+	printmap(tmp);
+	go_left_till_wall(tmp, row, column);
+	printmap(tmp);
+	go_up_till_wall(tmp, row, column);
+	printmap(tmp);
+	go_down_till_wall(tmp, row, column);
+	printmap(tmp);
+	//these work? now make it so they are called in the corresponding conditions
+	//call down if top/right are walls, and if left is painted etc
 }
