93822c697d378802190c1a27025a440543e4b951
Dancode52 <daniellanehart@gmail.com>
2026-03-02 15:40:17 +0100

    bonus functions

---
+++ headers/so_long_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 15:03:49 by dlanehar          #+#    #+#             */
+/*   Updated: 2026/03/02 12:45:35 by dlanehar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "./libft.h"
+# include "./get_next_line_bonus.h"
+# include "./mlx.h"
+# include "./mlx_extended.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include <string.h>
+# include "./so_long.h"
+
+
+
+#endif
