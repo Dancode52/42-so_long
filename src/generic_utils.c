eaf4d560b8cf1ceaa73ca3d8a3e28e2d29fe2f34
Dancode52 <daniellanehart@gmail.com>
2026-03-03 15:19:33 +0100

    creation and implementation of helper functions for image loading

--- src/generic_utils.c
+++ src/generic_utils.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 11:34:51 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/02/20 11:29:45 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/03/03 15:16:40 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	free_memory(char **memory)
 	int	i;
 
 	i = 0;
+	if (!memory)
+		return ;
 	while (memory[i])
 	{
 		free(memory[i]);
@@ -40,4 +42,34 @@ void	printmap(char **map)
 	i = 0;
 	while (map[i])
 		ft_printf("%s\n", map[i++]);
+}
+
+mlx_image	load_image(t_game_state *game, char *str)
+{
+	mlx_image	img;
+
+	img = mlx_new_image_from_file(game->mlx, str, &game->img_w, &game->img_h);
+	if (!img)
+	{
+		ft_printf("Error\nTexture loading failed\n");
+		free_memory(game->map);
+		free_map_info(&game->map_info);
+		exit(EXIT_FAILURE);
+	}
+	return (img);
+}
+
+mlx_window	load_window(t_game_state *game)
+{
+	mlx_window	win;
+
+	win = mlx_new_window(game->mlx, &game->win_info);
+	if (!win)
+	{
+		free_memory(game->map);
+		free_map_info(&game->map_info);
+		ft_printf("Error\nWindow loading failed\n");
+		exit(EXIT_FAILURE);
+	}
+	return (win);
 }
