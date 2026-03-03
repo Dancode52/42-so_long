976bd570ec4f2a5af853c4a73fa5880a4685323c
Dancode52 <daniellanehart@gmail.com>
2026-03-03 15:20:03 +0100

    addition of protection for null mlx

--- src/t_game_init.c
+++ src/t_game_init.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 09:16:12 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/03/02 12:58:59 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/03/03 13:01:01 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,43 +25,27 @@ void	game_init(t_game_state *game, char **map, t_map_count m_inf)
 // ---- add fail for no texture
 void	game_image_player_init(t_game_state *game)
 {
-	game->img_p_down = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/krobusdown.png", &game->img_w, &game->img_h);
-	game->img_p_up = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/krobusup.png", &game->img_w, &game->img_h);
-	game->img_p_left = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/krobusleft.png", &game->img_w, &game->img_h);
-	game->img_p_right = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/krobusright.png", &game->img_w, &game->img_h);
+	game->img_p_down = load_image(game, "textures/stardew/krobusdown.png");
+	game->img_p_up = load_image(game, "textures/stardew/krobusup.png");
+	game->img_p_left = load_image(game, "textures/stardew/krobusleft.png");
+	game->img_p_right = load_image(game, "textures/stardew/krobusright.png");
 }
 
 // ---- add fail for no texture
 void	game_image_ground_init(t_game_state *game)
 {
-	game->img_lwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/leftwall.png", &game->img_w, &game->img_h);
-	game->img_rwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/rightwall.png", &game->img_w, &game->img_h);
-	game->img_twall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/topwall.png", &game->img_w, &game->img_h);
-	game->img_bwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/bottomwall.png", &game->img_w, &game->img_h);
-	game->img_tlwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/topleft.png", &game->img_w, &game->img_h);
-	game->img_trwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/topright.png", &game->img_w, &game->img_h);
-	game->img_blwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/bottomleft.png", &game->img_w, &game->img_h);
-	game->img_brwall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/bottomright.png", &game->img_w, &game->img_h);
-	game->img_1wall = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/1wall.png", &game->img_w, &game->img_h);
-	game->img_floor = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/16_16.png", &game->img_w, &game->img_h);
-	game->img_egg = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/egg.png", &game->img_w, &game->img_h);
-	game->img_exit = mlx_new_image_from_file(game->mlx,
-			"textures/stardew/exit.png", &game->img_w, &game->img_h);
+	game->img_lwall = load_image(game, "textures/stardew/leftwall.png");
+	game->img_rwall = load_image(game, "textures/stardew/rightwall.png");
+	game->img_twall = load_image(game, "textures/stardew/topwall.png");
+	game->img_bwall = load_image(game, "textures/stardew/bottomwall.png");
+	game->img_tlwall = load_image(game, "textures/stardew/topleft.png");
+	game->img_trwall = load_image(game, "textures/stardew/topright.png");
+	game->img_blwall = load_image(game, "textures/stardew/bottomleft.png");
+	game->img_brwall = load_image(game, "textures/stardew/bottomright.png");
+	game->img_1wall = load_image(game, "textures/stardew/1wall.png");
+	game->img_floor = load_image(game, "textures/stardew/16_16.png");
+	game->img_egg = load_image(game, "textures/stardew/egg.png");
+	game->img_exit = load_image(game, "textures/stardew/exit.png");
 }
 
 void	game_win_init(t_game_state *game)
@@ -69,14 +53,21 @@ void	game_win_init(t_game_state *game)
 	game->win_info.title = "so_long";
 	game->win_info.height = game->tile_size_px * game->map_info.map_height;
 	game->win_info.width = game->tile_size_px * game->map_info.map_width;
-	game->win = mlx_new_window(game->mlx, &game->win_info);
+	game->win = load_window(game);
 }
 
 void	game_base_init(t_game_state *game, char **map, t_map_count m_inf)
 {
 	ft_bzero(game, sizeof(t_game_state));
-	game->map_info = m_inf;
 	game->mlx = mlx_init();
+	if (game->mlx == NULL)
+	{
+		free_memory(map);
+		free_map_info(&m_inf);
+		ft_printf("Error\nmlx_init failed\n");
+		exit(EXIT_FAILURE);
+	}
+	game->map_info = m_inf;
 	game->map = map;
 	game->tile_size_px = 64;
 }
